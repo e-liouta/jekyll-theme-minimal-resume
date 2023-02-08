3ff594b1c9bf0a456ca27e3862febed06505541e
e-liouta <effieliouta@gmail.com>
2023-02-08 14:56:12 +0200

    gitflow and dependencies

---
+++ generate-pdf.js
@@ -0,0 +1,29 @@
+<button id="generate-pdf-button">Generate PDF</button>
+
+<script>
+  document.getElementById('generate-pdf-button').addEventListener('click', async function() {
+    const puppeteer = require('puppeteer');
+    const fs = require('fs');
+
+    async function generatePdf() {
+      const htmlFiles = ['_includes/about.html'];
+      let htmlContent = '';
+
+      for (const file of htmlFiles) {
+        htmlContent += fs.readFileSync(file, 'utf8');
+      }
+
+      const browser = await puppeteer.launch();
+      const page = await browser.newPage();
+      await page.setContent(htmlContent);
+      await page.pdf({
+        path: 'cv.pdf',
+        format: 'A4',
+        printBackground: true
+      });
+      await browser.close();
+    }
+
+    generatePdf();
+  });
+</script>
